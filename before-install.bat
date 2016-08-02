614bc94d4b37e054d2885f1e7738551ef8059400
zhcloud <bj.zhaoyun@gmail.com>
2016-08-02 23:34:23 +0800

    Fix bug

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,3 @@
 REM Install Internet Information Server (IIS). 
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
